b67be45d961b8469593ef2d6ff0b8fd3a1c716b5
jgw0915 <121889006+jgw0915@users.noreply.github.com>
2024-10-13 18:45:21 +0800

    Feat/Add TFLite Object Detect Model Functionality  and Change Expo-Camera to Vision-Camera

--- src/screens/ScreenBrokenDetect.tsx
+++ src/screens/ScreenBrokenDetect.tsx
@@ -1,22 +1,87 @@
 import { Ionicons } from '@expo/vector-icons';
-import { CameraType, CameraView, useCameraPermissions } from 'expo-camera';
 import * as ImageManipulator from 'expo-image-manipulator';
 import React, { useEffect, useRef, useState } from 'react';
 import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useTensorflowModel } from 'react-native-fast-tflite';
 import { NativeStackScreenProps } from 'react-native-screens/lib/typescript/native-stack/types';
+import { Camera, useCameraDevice, useCameraPermission, useFrameProcessor } from 'react-native-vision-camera';
+import { useResizePlugin } from 'vision-camera-resize-plugin';
 import { rootStackParams } from '../Navigator/stack/StackNavigator';
-
+import { CLASSES } from '../data/classes';
 
 type props = NativeStackScreenProps<rootStackParams, 'ScreenBrokenDetect'>;
 
 const ScreenBrokenDetectScreen = ({navigation, route}:props) => {
     const [timeLeft, setTimeLeft] = useState(6);
-    const [facing, setFacing] = useState<CameraType>('front');
-    const [permission, requestPermission] = useCameraPermissions();
+    // const [facing, setFacing] = useState<CameraType>('front');
+    const { hasPermission, requestPermission } = useCameraPermission()
+    const device = useCameraDevice('front')!;
+
+    
+    const objectDetection = useTensorflowModel(require('@Assets/Models/object_detect.tflite'))
+    const model = objectDetection.state === "loaded" ? objectDetection.model : undefined
+
+    const { resize } = useResizePlugin()
+
+    const frameProcessor = useFrameProcessor((frame:any) => {
+        'worklet'
+        if (model == null) {
+            return
+        }
+        // const canvas = new SkCanvas(frame);
+        // 1. Resize 4k Frame to 320x320x3 using vision-camera-resize-plugin
+        const resized = resize(frame, {
+            scale: {
+            width: 320,
+            height: 320,
+            },
+            pixelFormat: 'rgb',
+            dataType: 'uint8',
+        })
+
+        // 2. Run model with given input buffer synchronously
+        const outputs = model.runSync([resized])
+
+        // 3. Interpret outputs accordingly
+        const detection_boxes = outputs[0]
+        const detection_classes = outputs[1]
+        const detection_scores = outputs[2]
+        const num_detections = outputs[3]
+        console.log(`Detected ${num_detections[0]} objects!`)
+
+        for (let i = 0; i < detection_boxes.length; i += 4) {
+            const confidence = detection_scores[i / 4]
+            const detected_class_index = detection_classes[i / 4]
+            if (confidence > 0.7) {
+                console.log(`Detected object with confidence ${confidence}`)
+                console.log(`Class: ${detected_class_index}`)
+                // 4. Draw a red box around the detected object!
+                const left = detection_boxes[i]
+                const top = detection_boxes[i + 1]
+                const right = detection_boxes[i + 2]
+                const bottom = detection_boxes[i + 3]
+                // const rect = SkRect.Make(left, top, right, bottom)
+                // canvas.drawRect(rect, SkColors.Red)
+                if (CLASSES[Number(detected_class_index)].displayName === 'cell phone') {
+                    useEffect(() => {
+                        if (timeLeft <= 0) {
+                            onTimeOut();
+                            return;
+                        }
+                        
+                        const timerId = setInterval(() => {
+                            setTimeLeft((prevTime:any) => prevTime - 1);
+                        }, 1000);
+                    
+                        return () => clearInterval(timerId); // Cleanup on unmount or when timeLeft changes
+                        }, [timeLeft, onTimeOut]);
+                }
+            }
+        }
+    }, [model])
 
     const onTimeOut = async() => {
         const pic = await _takePicture();
-        // console.log("pic = ",pic);
         navigation.replace("ScreenBrokenDetectResult", {
             userData: route.params.userData,
             phoneName: route.params.phoneName,
@@ -26,7 +91,7 @@ const ScreenBrokenDetectScreen = ({navigation, route}:props) => {
         });
     }
 
-    let camera = useRef<CameraView>(null);
+    let camera = useRef<Camera>(null);
 
 
     const _takePicture = async () => {
@@ -35,8 +100,8 @@ const ScreenBrokenDetectScreen = ({navigation, route}:props) => {
             console.log("Taking Picture");
 
         // Take the picture
-        const options = { quality: 1, base64: true, skipProcessing: false };
-        let photo = await camera.current.takePictureAsync(options);
+        // const options = { quality: 1, base64: true, skipProcessing: false };
+        let photo = await camera.current.takePhoto();
 
         // Determine the crop area dimensions
         // These values should be calculated based on the rectangle area on the screen
@@ -47,7 +112,7 @@ const ScreenBrokenDetectScreen = ({navigation, route}:props) => {
 
         // Crop the image using ImageManipulator
         const croppedImage = await ImageManipulator.manipulateAsync(
-            photo!.uri,
+            photo.path!,
             [
                 {
                     crop: {
@@ -66,39 +131,22 @@ const ScreenBrokenDetectScreen = ({navigation, route}:props) => {
     }
     };
 
-    useEffect(() => {
-        if (timeLeft <= 0) {
-            onTimeOut();
-            return;
-        }
-        
-        const timerId = setInterval(() => {
-            setTimeLeft((prevTime:any) => prevTime - 1);
-        }, 1000);
-    
-        return () => clearInterval(timerId); // Cleanup on unmount or when timeLeft changes
-        }, [timeLeft, onTimeOut]);
-    
-    if (!permission) {
-        // Camera permissions are still loading.
-        return(
-            <View>
-                <Text>Camera permissions are still loading.</Text>
-            </View>
-        )
-    }
 
-    if (!permission?.granted) {
+    if (!hasPermission) {
         Alert.alert('Camera permission required', 'Please allow camera permission to proceed.');
         requestPermission();
         return <Text>No access to camera</Text>;
     }
 
+    // if (device == null) return <Text />
     return (
-        <CameraView
-            facing={facing}
+        <Camera
             style={styles.background}
-            ref ={camera}
+            device={device}
+            isActive={true}
+            frameProcessor={frameProcessor}
+            ref={camera}
+            photo={true}
         >
             <View style={styles.overlay}>
                 {/* Top overlay part above the rectangle */}
@@ -128,7 +176,7 @@ const ScreenBrokenDetectScreen = ({navigation, route}:props) => {
                     Please use the front camera to place your phone within the rectangle and wait for 5 seconds.
                 </Text>
             </View>
-        </CameraView>
+        </Camera>
     );
 };
 
