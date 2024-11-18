ffdf7d3f1fc8d8f9bf45dc9b4c3297ab062f402d
jgw0915 <jgw.0915.tpc@gmail.com>
2024-11-18 12:38:18 +0800

    Refactor/Adding Test Class Label

--- src/Screens/ScreenBrokenDetect.tsx
+++ src/Screens/ScreenBrokenDetect.tsx
@@ -7,7 +7,7 @@ import { NativeStackScreenProps } from 'react-native-screens/lib/typescript/nati
 import { Camera, useCameraDevice, useCameraPermission, useFrameProcessor } from 'react-native-vision-camera';
 import { useResizePlugin } from 'vision-camera-resize-plugin';
 import { rootStackParams } from '../Navigator/stack/StackNavigator';
-import { CLASSES } from '../data/classes';
+// import { CLASSES } from '../data/classes';
 
 type props = NativeStackScreenProps<rootStackParams, 'ScreenBrokenDetect'>;
 
@@ -16,9 +16,19 @@ const ScreenBrokenDetectScreen = ({navigation, route}:props) => {
     // const [facing, setFacing] = useState<CameraType>('front');
     const { hasPermission, requestPermission } = useCameraPermission()
     const device = useCameraDevice('front')!;
-    const [isTimerActive, setIsTimerActive] = useState(true);
+    const [isTimerActive, setIsTimerActive] = useState(false);
 
-    const classIndices = getClassIndices(CLASSES);
+    const CLASSES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 
+        'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 
+        'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 
+        'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 
+        'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 
+        'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 
+        'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 
+        'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 
+        'hair drier', 'toothbrush'];
+
+    // const classIndices = getClassIndices(CLASSES);
     // console.log(`CLASSES array:`, CLASSES);
     
     const objectDetection = useTensorflowModel(require('@Assets/Models/ssd-mobilenet-v1-tflite-default-v1.tflite'))
@@ -70,13 +80,13 @@ const ScreenBrokenDetectScreen = ({navigation, route}:props) => {
         for (let i = 0; i < detection_boxes.length; i += 4) {
             const confidence = detection_scores[i / 4]
             const detected_class_index = detection_classes[i / 4]
-            // console.log(`Detected object with confidence ${confidence}`)
-            // console.log(`Detected class index: ${detected_class_index}`);
+            console.log(`Detected object with confidence ${confidence}`)
+            console.log(`Detected class index: ${detected_class_index}`);
             
-            if (!classIndices.includes(Number(detected_class_index)) && CLASSES[Number(detected_class_index)]) {
-                const detect_object = CLASSES[Number(detected_class_index)].displayName;
-                // console.log(`Detected object with confidence ${confidence}`);
-                // console.log(`Class: ${detect_object}`);
+            if (detected_class_index < CLASSES.length) {
+                const detect_object = CLASSES[Number(detected_class_index)];
+                console.log(`Detected object with confidence ${confidence}`);
+                console.log(`Class: ${detect_object}`);
                 if (confidence > 0.7) {
                     // 4. Draw a red box around the detected object!
                     const left = detection_boxes[i];
@@ -106,13 +116,13 @@ const ScreenBrokenDetectScreen = ({navigation, route}:props) => {
     }
 
     const onTimeOut = async() => {
-        const pic = await _takePicture();
+        // const pic = await _takePicture();
         navigation.replace("ScreenBrokenDetectResult", {
             userData: route.params.userData,
             phoneName: route.params.phoneName,
             phoneType: route.params.phoneType,
             phoneNumber: route.params.phoneNumber,
-            takenPhoto: pic,
+            takenPhoto: "https://png.pngtree.com/png-clipart/20230303/original/pngtree-broken-phone-screen-design-png-image_8971601.png",
         });
     }
 
